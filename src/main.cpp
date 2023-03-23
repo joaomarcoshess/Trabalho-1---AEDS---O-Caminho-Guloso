48450c502b007d212f64ddd21fbe7947a61ff97a
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2023-03-23 20:13:37 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@ using namespace std;
 int main() 
 {
   Matriz m;
-  m.leArquivo();
+  
+  cout <<"O Caminho Guloso" << endl << endl;
+  cout <<"O Caminho Guloso determina o maior número entre 5 direcoes, no qual" << endl;
+  cout <<"o caminho sera demarcado pelo número -1 e no final sera mostrado o" << endl;
+  cout <<"caminho feito, alem de mostrar a soma de todos os numeros no final." << endl << endl;
+  
+  m.LerArquivo();
   return 0;
 }
