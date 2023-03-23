014bf13c9b6afe1f433e98ae6d1e0e93cab3d27b
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2023-03-23 18:43:48 -0300

    Update Matriz.hpp

--- src/Matriz.hpp
+++ src/Matriz.hpp
@@ -28,26 +28,3 @@ class Matriz
 
 
 
-/*#include <bits/stdc++.h>
-
-#ifndef MATRIZ_HPP_INCLUDED
-#define MATRIZ_HPP_INCLUDED
-
-using namespace std
-
-class Matriz{
-  private
-    int **matriz;
-
-  public:
-    Matriz(int **matriz)
-    Matriz();
-
-    void setMatriz(int **matriz);
-    int **getMatriz();
-
-    int **limparMatriz(int **matriz, int tam);
-    int percorrerMatriz(int **matriz, int tam);
-    int mostrarCaminho(int **matriz, int tam);
-};
-#endif*/
